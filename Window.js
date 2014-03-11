5d007b8f087f762d2290b33114ac6c627a11c27e
jimjonesbr <jimjonesbr@gmail.com>
2014-03-11 14:20:08 +0100

    Window class added.

---
+++ Window.js
@@ -0,0 +1,11 @@
+
+
+function Window(){ }
+
+Window.prototype.upperLeftLatitude = 0.0;
+Window.prototype.upperLeftLongitude = 0.0;
+Window.prototype.lowerRightLatitude = 0.0;
+Window.prototype.lowerRightLongitude = 0.0;
+Window.prototype.variable = "";
+
+
