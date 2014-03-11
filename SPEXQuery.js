120f8d567ab3f6a93b3b00b829c06a9d9c6f1452
simonscheider <simonscheider@web.de>
2014-03-11 12:08:08 +0100

    query

--- SPEXQuery.js
+++ SPEXQuery.js
@@ -1,8 +1,8 @@
 
 
 function SPEXQuery(){ }
-
-SPEXQuery.prototype= new Query();
+SPEXQuery.prototype = new Query();
+SPEXQuery.prototype.constructor = SPEXQuery;
 SPEXQuery.prototype.spatialConstraints = "";
 SPEXQuery.prototype.temporalConstraints = "";
 SPEXQuery.prototype.thematicConstraints = "";
