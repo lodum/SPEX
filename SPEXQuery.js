312a5128a76dc4d929bf6dc097b0a4cf47098758
jimjonesbr <jimjonesbr@gmail.com>
2014-03-11 12:05:04 +0100

    SPEXQuery added.

---
+++ SPEXQuery.js
@@ -0,0 +1,11 @@
+
+
+function SPEXQuery(){ }
+
+SPEXQuery.prototype=new Query(); SPEXQuery
+SPEXQuery.prototype.spatialConstraints = "";
+SPEXQuery.prototype.temporalConstraints = "";
+SPEXQuery.prototype.thematicConstraints = "";
+SPEXQuery.prototype.getSPARQL= function (){ return null; }
+
+
