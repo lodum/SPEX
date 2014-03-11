a04d3f844336bdf429a5b8370a0d1e575bc3cdab
simonscheider <simonscheider@web.de>
2014-03-11 14:53:59 +0100

    q

--- SPEXQuery.js
+++ SPEXQuery.js
@@ -5,8 +5,12 @@ SPEXQuery.prototype.constructor = SPEXQuery;
 SPEXQuery.prototype.spatialConstraints = [];
 SPEXQuery.prototype.temporalConstraints = [];
 SPEXQuery.prototype.thematicConstraints = [];
-SPEXQuery.prototype.getSPARQL= function (){ return null; }
+SPEXQuery.prototype.getSPARQL= function (){ return this.serialiseQuery();}
 
 SPEXQuery.prototype.setSpatialConstraint = function(va, window){
 this.spatialConstraints.push({ "v" : va, "w" : window });
 }
+SPEXQuery.prototype.setTemporalConstraint = function(va, constr){
+this.spatialConstraints.push({ "v" : va, "t" : constr});
+}
+
