4c4e4c9524b3ee8efd678edde62f66125afbf3cf
simonscheider <simonscheider@web.de>
2014-03-11 12:08:56 +0100

    q

--- SPEXQuery.js
+++ SPEXQuery.js
@@ -3,9 +3,9 @@
 function SPEXQuery(){ }
 SPEXQuery.prototype = new Query();
 SPEXQuery.prototype.constructor = SPEXQuery;
-SPEXQuery.prototype.spatialConstraints = "";
-SPEXQuery.prototype.temporalConstraints = "";
-SPEXQuery.prototype.thematicConstraints = "";
+SPEXQuery.prototype.spatialConstraints = [];
+SPEXQuery.prototype.temporalConstraints = [];
+SPEXQuery.prototype.thematicConstraints = [];
 SPEXQuery.prototype.getSPARQL= function (){ return null; }
 
 
