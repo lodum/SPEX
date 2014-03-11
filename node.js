54dc0e12fa10110a7164bae6d10d32224b286f9c
simonscheider <simonscheider@web.de>
2014-03-11 14:39:10 +0100

    node

---
+++ node.js
@@ -0,0 +1,6 @@
+function Node(id){
+this.id = id;
+this.label = ""; //can be any short string for the node
+this.getID = function (){return this.id};
+this.getLabel = function (){return this.label};
+ }
