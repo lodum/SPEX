a7c70de0d6a53eb81e15c253555f4bc360264338
simonscheider <simonscheider@web.de>
2014-03-11 15:06:30 +0100

    y

--- node.js
+++ node.js
@@ -1,6 +1,7 @@
 function Node(id){
-this.id = id;
-this.label = ""; //can be any short string for the node
+this.id = id; //this is the node identifier (a string, e.g. URI)
+this.label = ""; //can be any short name for the node
 this.getID = function (){return this.id};
 this.getLabel = function (){return this.label};
+this.prototype.toString = function() {return this.id;}
  }
