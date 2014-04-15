ade71e4072ba1cd8f54972046eb381625963c4d2
simonscheider <simonscheider@web.de>
2014-04-15 20:00:38 +0200

    diverse query libraries

--- QueryExecutor.js
+++ QueryExecutor.js
@@ -7,20 +7,59 @@ QueryExecutor.prototype.resultsLimit = 50;
 QueryExecutor.prototype.timeout = 5000;
 
 
-
 QueryExecutor.prototype.executeQuery = function(spexquery, endpoint) {
 spexquery.limit = this.resultsLimit;
-spexquery.config.endpoint = endpoint;
-spexquery.execute(callbackqe);
+this.sparqlQueryJson(spexquery.getSPARQL(), endpoint, this.callback, true);
+}
 
-function callbackqe(results){
-if(results = []){
-console.log("Empty result set");
-} else {
-return results;
+QueryExecutor.prototype.callback = function(str){
+var jsonObj = eval('(' + str + ')');
+var fjsonObj = this.filterResults(jsonObj);
+this.resultspane.display(fjsonObj);
 }
+
+QueryExecutor.prototype.filterResults = function(resultset){
+return resultset;
 }
-}
+
+QueryExecutor.prototype.sparqlQueryJson = function(queryStr, endpoint, callback, isDebug) {
+      var querypart = "query=" + escape(queryStr);
+    
+      // Get our HTTP request object.
+      var xmlhttp = null;
+      if(window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest();
+     } else if(window.ActiveXObject) {
+       // Code for older versions of IE, like IE6 and before.
+       xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+     } else {
+       alert('Perhaps your browser does not support XMLHttpRequests?');
+     }
+    
+     // Set up a POST with JSON result format.
+     xmlhttp.open('POST', endpoint, true); // GET can have caching probs, so POST
+     xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+     xmlhttp.setRequestHeader("Accept", "application/sparql-results+json");
+    
+     // Set up callback to get the response asynchronously.
+     xmlhttp.onreadystatechange = function() {
+       if(xmlhttp.readyState == 4) {
+         if(xmlhttp.status == 200) {
+           // Do something with the results
+           if(isDebug) alert(xmlhttp.responseText);
+           callback(xmlhttp.responseText);
+         } else {
+           // Some kind of error occurred.
+           alert("Sparql query error: " + xmlhttp.status + " "
+               + xmlhttp.responseText);
+         }
+       }
+     };
+     // Send the query to the endpoint.
+     xmlhttp.send(querypart);
+    
+     // Done; now just wait for the callback to be called.
+    };
 
 
 
--- ResultsPane.js
+++ ResultsPane.js
@@ -1,2 +1,23 @@
 function ResultsPane(){}
 ResultsPane.prototype.constructor = ResultsPane;
+ResultsPane.prototype.display = function(json){
+		var html = "<table border='1'>";
+		html +="<tr>";
+		for (var x in json.head.vars) {		
+		html +="<td>" +json.head.vars[x]+ "</td>";
+		}
+		html += "</tr>";
+        for (var b in json.results.bindings) {
+            html += "<tr>";
+            for (var x in json.head.vars) {
+                var value = json.results.bindings[b][json.head.vars[x]];
+                if (value.type == "uri")
+                    html += "<td><a href='"+value.value+"'>" + value.value + "</a></td>";
+                else
+                    html += "<td>" + value.value + "</td>";
+            }
+            html += "</tr>";
+        }
+        html += "</table>";
+        document.getElementById("result").innerHTML = html;
+}
