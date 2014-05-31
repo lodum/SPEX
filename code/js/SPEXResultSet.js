8716cab16ce36af567dfec01eae1c546bdecd1f2
NemanjaKostic <kostic_nemanja@yahoo.com>
2014-05-31 15:52:48 +0200

    Update SPEXResultSet.js

--- code/js/SPEXResultSet.js
+++ code/js/SPEXResultSet.js
@@ -1,6 +1,7 @@
 function SPEXResultSet(json) {
 	this.allResults = json;
 	this.labeledResults = [];
+	this.userSelectedVariables = [];
 }
 
 
@@ -12,13 +13,21 @@ SPEXResultSet.prototype.getAllResults = function() {
 	return this.allResults;
 };
 
+/* Function to return properly labeled results ready for display. */
+SPEXResultSet.prototype.getResultsForDisplay = function() {
+	return this.labeledResults;
+};
+
 /*Function to add new solution into result set. */
 SPEXResultSet.prototype.addNewResult = function(resultObject) { //is this what it's meant to do?
 	this.allResults.results.bindings.push(resultObject);
+	//If a new solution has been added, we have to reformat the labeledResults attribute.
+	this.prepareResultsForDisplay();
 }; 
 
-/* Function to extract instance-label pairs from JSON result and store them as array of objects, for use in displaying results. */
-SPEXResultSet.prototype.storeLabeledResults = function() {
+/* Function to extract instance-label pairs from JSON result that has been worked on by LabelGenerator,
+   and store the pairs as array of objects, for use in displaying results. */
+SPEXResultSet.prototype.prepareResultsForDisplay = function() {
 	var solutions = this.allResults.results.bindings;
 	for(var i = 0; i < solutions.length; i++) {
 		for(variableName in solutions[i]) {
