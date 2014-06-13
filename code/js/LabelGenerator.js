3d231732d03d8ea98bbc23760ced1525ca69a008
NemanjaKostic <kostic_nemanja@yahoo.com>
2014-06-13 20:01:05 +0200

    Update LabelGenerator.js

--- code/js/LabelGenerator.js
+++ code/js/LabelGenerator.js
@@ -5,18 +5,22 @@ function LabelGenerator() {}
 LabelGenerator.prototype.label = function(resultSet) {
 	
 	/* Define reference points. */
-	var variables = resultSet.getAllResults().head.vars;
-	var data = resultSet.getAllResults().results.bindings;
+	var variables = resultSet.getAllResults().head.vars; 
+	var solutions = resultSet.getAllResults().results.bindings;
 	
 	for(var i = 0; i < variables.length; i++){
+		var v = variables[i];
 		/*Check if the variable is a __label variable. */
-		if(variables[i].substring(variables[i].length - 7, variables[i].length) === "__label"){
+		if(v.substring(v.length - 7, v.length) === "__label"){
 			/*For each solution, check if the __label variable is bound that solution; 
 			if not, create a binding (i.e. add a member corresponding to the __label variable to the solution object).
 			(A variable does not appear in a solution object if it is not bound in that particular query solution.) */
-			for(var j = 0; j < data.length; j++){				
-				if(!data[j][variables[i]]){
-					data[j][variables[i]] = {"type": "literal", "value": createLabel(data[j][variables[i].substring(0, variables[i].length - 7)])};
+			for(var j = 0; j < solutions.length; j++){				
+				var sol = solutions[j];
+				if(!sol[v]){
+					if(sol[v.substring(0, v.length - 7)]) {
+						sol[v] = {"type": "literal", "value": createLabel(sol[v.substring(0, v.length - 7)])};
+					}
 				} 
 			}
 		}
@@ -27,8 +31,6 @@ LabelGenerator.prototype.label = function(resultSet) {
 	/* Function to create a label for a variable according to its type. */
 	function createLabel(variable) {
 		if(variable.type === "uri") {
-			//TO BE EDITED: in this case it should be a html link which links to the URI
-			// Very simple, only problem: how to handle quotes in strings? e.g. "<a href="">"
 			return handleURI(variable.value);
 		} else if(variable.type === "literal" || variable.type === "typed-literal") { 													//DO WE NEED THIS?
 			return variable.value;
