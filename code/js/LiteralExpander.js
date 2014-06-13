326985f11dcd8fb74a684e1b34fbbaa193745c3c
NemanjaKostic <kostic_nemanja@yahoo.com>
2014-06-13 14:15:26 +0200

    Update LiteralExpander.js

--- code/js/LiteralExpander.js
+++ code/js/LiteralExpander.js
@@ -1,28 +1,11 @@
 /* Constructor function for LiteralExpander objects. */
 function LiteralExpander() {
-	this.listOfFilterLiteralTypes  = "";
-	this.listOfLabelProperties = [];	
+	this.listOfLabelProperties = ["rdfs:label", "dct:title", "dc:title", "foaf:name"];	
 }
 
-/* Function to add triples to retrieve labels for SELECT statement variables. */
-LiteralExpander.prototype.expandLabels = function(query) {  
-	//See if preliminary query has any variables in SELECT statement; if not, parse serialized query for all variables.
-	if(query.variables.length !== 0) {
-		return labelExpander(query);
-	} else {	
-		var queryAsString = query.getSPARQL();//query.serialiseQuery();
-		var pattern = /(?:\?|\$)[^ |^.|^)]*/g;  												//ADD [^}] ?
-		var variableList = queryAsString.match(pattern);
-		reduce(variableList);
-		return labelExpander(query, variableList);		
-	}
-	
-	function labelExpander(query, variableList){
+/* Function to add triples to retrieve labels for user-selected query variables. */
+LiteralExpander.prototype.expandLabels = function(spexquery) {  
 		//Check if there are original __label variables!!
-		
-		if(!variableList) {
-			var variableList = query.variables;
-		}
 
 		//Construct string to use as predicate of triples that ask for labels.
 		var labelPredicate = "";
@@ -34,41 +17,18 @@ LiteralExpander.prototype.expandLabels = function(query) {
 			}
 		}
 		
-		//Add OPTIONAL triple patterns inside query's WHERE statement.
-		for (var k = 0; k < variableList.length; k++){
+		//Add OPTIONAL triple patterns to query, asking for labels of user-selected query variables.
+		for (var k = 0; k < spexquery.SPEXvariables.length; k++){
 			//Syntax of the predicate: see "http://www.w3.org/TR/sparql11-query/#propertypath-examples".
 			//Multiple results for label should be handled!!
-			query.optional().where(variableList[k], labelPredicate, variableList[k] + "__label");
+			spexquery.optional().where(spexquery.SPEXvariables[k], labelPredicate, spexquery.SPEXvariables[k] + "__label");
 		}
 		
-		//If query's SELECT statement is not empty (i.e. query.variables is not empty array), 
-		//add corresponding label variables to query.variables, to use for display of query results.
-		if(query.variables.length !== 0) {
-			for (var k = 0; k < query.variables.length; k++) {
-				query.variables.push(query.variables[k] + "__label");
-			}
-		}
-		
-		return query;
-	}
+		return spexquery;
 
-	//Function to remove duplicates in the array of SPARQL variables.
-	function reduce(list) {
-		for(var i = 0; i < list.length; i++){
-			for(var j = i + 1; j < list.length; j++){
-				//IF statement doesn't look at first character, it could be "$" or "?".
-				if(list[i].substring(1, list[i].length) === list[j].substring(1, list[j].length)){
-					list.splice(j, 1);
-					j--;
-				}
-			}
-		}
-	}
-	
 };
-
-LiteralExpander.prototype.expandFilterLiterals = function(query) {};
 
 LiteralExpander.prototype.addLabelProperty = function(property) {
 	this.listOfLabelProperties.push(property);
 };
+
