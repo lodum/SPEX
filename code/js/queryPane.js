8b65c96bb87ae501121bff307332cbdf0ef1ce2c
zimmp <peterzimmerhof@googlemail.com>
2014-06-17 10:45:19 +0200

    Intermediate queryPane commit

--- code/js/queryPane.js
+++ code/js/queryPane.js
@@ -1,8 +1,8 @@
 var queryPane = new function(){
 
 	// var
-	this.width = '100%',
-	this.height = '100%';
+	this.width = '500',
+	this.height = '700';
 
 	this.colors = d3.scale.category10();
 
@@ -13,12 +13,16 @@ var queryPane = new function(){
 	this.path = null;
 	this.pathText = null;
 
-	this.nodes = [{id: 0, name: '?', x: 100, y: 50},
-				{id: 1, name: 'Person', x: 30, y: 100},
-				{id: 2, name: 'Map', x: 100, y: 150}];
 
-	this.links = [{id: 0, name: 'a', source: this.nodes[0], target: this.nodes[1]},
-				{id: 1, name: 'a', source: this.nodes[0], target: this.nodes[2]}];
+	this.visPath = null;
+
+    this.visPathText = null;
+
+    this.visNode = null;
+
+	this.nodes = [{id: 0, name: '?', x: 0, y: 0}];
+
+	this.links = [];
 
 	this.selected = null;
 
@@ -32,10 +36,11 @@ var queryPane = new function(){
 		// Declare a spot for the graph
 		this.vis = d3.select("#queryPane").append("svg")
 		.attr({
-	        "width": this.width,
-	        "height": this.height
-      	})
-      	.attr("preserveAspectRatio", "xMidYMid meet")
+	        "width": "100%",
+	        "height": "100%"
+	      })
+		//.attr("viewBox", "0 0 " + this.width + " " + this.height )
+      	.attr("preserveAspectRatio", "xMidYMid")
       	.attr("pointer-events", "all");
 	 
 	 	this.menu = d3.select("#contextMenu");
@@ -43,10 +48,10 @@ var queryPane = new function(){
 		// Create the graph
 		this.force = d3.layout.force()
 			.gravity(.05)
-			.distance(150)
-			.charge(-200)
-			.chargeDistance(150)
-			.on('tick', this.tick).size([500, 500]);
+			.distance(200)
+			.charge(-800)
+			.chargeDistance(300)
+			.on('tick', this.tick).size([700, 500]);
 	 
 
 		this.node_drag = d3.behavior.drag()
@@ -54,102 +59,18 @@ var queryPane = new function(){
         .on("drag", queryPane.dragmove)
         .on("dragend", queryPane.dragend);
 
-		// Add the data
+        // Add the data
 		this.force.nodes(this.nodes)
 			.links(this.links)
-			.start();	
+			.start();
 
-		this.path = this.vis.append('svg:g').selectAll(".link")
-		.data(this.links);
+		this.visPath = this.vis.append('svg:g');
 
-		// update existing links
-		this.path.classed('selected', function(d) { return false; })
-		  .style('marker-start', function(d) { return d.left ? 'url(#start-arrow)' : ''; })
-		  .style('marker-end', function(d) { return d.right ? 'url(#end-arrow)' : ''; });
+        this.visPathText = this.vis.append('svg:g');
 
+        this.visNode = this.vis.append('svg:g');
 
-		// add new links
-		gPath = this.path.enter().append('svg:path')
-		  .attr('class', 'link')
-		  .classed('selected', function(d) { return false; })
-		  .style('marker-start', function(d) { return d.left ? 'url(#start-arrow)' : ''; })
-		  .style('marker-end', function(d) { return d.right ? 'url(#end-arrow)' : ''; });
-
-		gPath.on("click", function(d) {
-        	queryPane.menu.datum(queryPane.selected = d).call(queryPane.contextMenu);
-    	});
-
-		//gPath.exit().remove();
-
-		  this.pathText = this.vis.append('svg:g').selectAll(".link")
-		  this.pathText = this.pathText.data(this.links);
-		this.pathText.enter().append('svg:text')
-		  .data(this.links)
-		  .attr('x', 20)
-		  .attr('y', 20)
-		  .text(function(d){return d.name;});
-
-		// remove old links
-		this.path.exit().remove();
-		this.pathText.exit().remove();
-
-		// // Draw the links
-		// this.link = this.vis.append('svg:g').selectAll(".link").data(this.force.links());
-	 
-		// // Update the new links
-	 // 	var gLink = this.link.enter().append('svg:g')
-
-		// gLink.append('svg:path')
-		//   .attr('class', 'link')
-		//   .classed('selected', function(d) { return false; })
-		//   .style('marker-start', function(d) { return d.left ? 'url(#start-arrow)' : ''; })
-		//   .style('marker-end', function(d) { return d.right ? 'url(#end-arrow)' : ''; })
-
-		// // Remove the old links
-		// this.link.exit().remove();
-	 
-
-
-		// Draw the nodes
-		this.node = this.vis.append('svg:g').selectAll(".node").data(this.nodes);
-	 
-
-		// Update the new nodes 
-		var g = this.node.enter().append("svg:g");
-		
-
-		var circ = g.append('svg:circle')
-	    .attr('class', 'node')
-	    .attr('r', 10)
-	    .style('fill', function(d) { 
-	    	return d3.rgb("#99e").brighter().toString(); 
-	    })
-	    .style('stroke', function(d) { 
-	    	return d3.rgb("#99e").darker().toString(); 
-	    });
-
-	    //g.exit().remove();
-	    //circ.exit().remove();
-
-	  	g.on("click", function(d) {
-	  		if (d3.event.defaultPrevented) return;
-        	queryPane.menu.datum(queryPane.selected = d).call(queryPane.contextMenu);
-    	});
-        // g.call(d3.behavior.drag()
-        // .on("drag", function(d) {
-
-        //   queryPane.menu.datum().call(queryPane.contextMenu);
-        //   return false;
-        // }));
-        g.call(this.node_drag);
-
-		g.append('svg:text')
-	      .attr('x', 15)
-	      .attr('y', 5)
-	      .attr('class', 'id')
-	      .text(function(d) { return d.name; });
-	    
-	    this.node.exit().remove();
+		this.update();
 
 	};
 
@@ -223,19 +144,100 @@ var queryPane = new function(){
     }
 
     this.add = function(){
-    	queryPane.nodes.push({id: queryPane.nodes.length, name: '?'});
-    	queryPane.links.push({id: queryPane.links.length, name: 'a', source: this.nodes[0], target: this.nodes[queryPane.nodes.length - 1]});
+    	queryPane.nodes.push({id: queryPane.nodes.length, name: '?'});// d3.select("#queryC").value });
+    	queryPane.links.push({id: queryPane.links.length, name: 'a',//d3.select("#queryP").html(this.value), 
+    		source: this.nodes.indexOf(queryPane.selected), target: this.nodes[queryPane.nodes.length - 1]});
 
-    	
-
-	    queryPane.node.exit().remove();
-
-		queryPane.path.exit().remove();
-
-	 
-
+	    this.update();
 
 	    this.force.start();
+    }
+
+    this.update = function() {
+
+    	this.path = this.visPath.selectAll(".link").data(this.links);
+
+        this.pathText = this.visPathText.selectAll(".link").data(this.links);
+
+        this.node = this.visNode.selectAll(".node").data(this.nodes);
+
+
+		// update existing links
+		this.path.classed('selected', false)
+		  .style('marker-start', function(d) { return d.left ? 'url(#start-arrow)' : ''; })
+		  .style('marker-end', function(d) { return d.right ? 'url(#end-arrow)' : ''; });
+
+
+		// add new links
+		gPath = this.path.enter().append('svg:path')
+		  .attr('class', 'link')
+		  .classed('selected', function(d) { return false; })
+		  .style('marker-start', function(d) { return d.left ? 'url(#start-arrow)' : ''; })
+		  .style('marker-end', function(d) { return d.right ? 'url(#end-arrow)' : ''; });
+
+		gPath.on("click", function(d) {
+        	queryPane.menu.datum(queryPane.selected = d).call(queryPane.contextMenu);
+    	});
+
+		//gPath.exit().remove();
+
+		
+		this.pathText.enter().append('svg:text')
+		  .data(this.links)
+		  .attr('class', 'link')
+		  .attr('x', 20)
+		  .attr('y', 20)
+		  .text(function(d){return d.name;});
+
+		// remove old links
+		this.path.exit().remove();
+		this.pathText.exit().remove()
+
+
+		// Draw the nodes
+		// Update the new nodes 
+		var g = this.node.enter().append("svg:g")
+		.attr('class', 'node')
+		.classed('selected', function(d) { return false; });
+		
+
+		var circ = g.append('svg:circle')
+	    .attr('r', 10)
+	    .style('fill', function(d) { 
+	    	return d3.rgb("#99e").brighter().toString(); 
+	    })
+	    .style('stroke', function(d) { 
+	    	return d3.rgb("#99e").darker().toString(); 
+	    });
+
+
+	  	g.on("click", function(d) {
+	  		if (d3.event.defaultPrevented) return;
+        	queryPane.menu.datum(queryPane.selected = d).call(queryPane.contextMenu);
+        	g.classed("selected", function(d) { return d === queryPane.selected; });
+    	});
+    	
+        // g.call(d3.behavior.drag()
+        // .on("drag", function(d) {
+
+        //   queryPane.menu.datum().call(queryPane.contextMenu);
+        //   return false;
+        // }));
+
+        g.call(this.node_drag);
+
+		g.append('svg:text')
+	      .attr('x', 15)
+	      .attr('y', 5)
+	      .attr('class', 'id')
+	      .text(function(d) { return d.name; });
+
+		g.classed("selected", function(d) { 
+	    	return d === queryPane.selected; 
+	    });
+	    
+	    this.node.exit().remove();
+
     }
 
 };
