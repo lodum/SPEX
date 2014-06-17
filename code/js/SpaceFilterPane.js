3a1327095aa0181c5520e5d0d9780d94263829a5
Jim Jones <jimjonesbr@gmail.com>
2014-06-17 16:56:35 +0200

    Update SpaceFilterPane.js
    Auto zoom-in implemented.

--- code/js/SpaceFilterPane.js
+++ code/js/SpaceFilterPane.js
@@ -7,13 +7,17 @@ function SpaceFilterPane() {
 SpaceFilterPane.prototype.displayGeometry = function(resultset){
 
 	var result = resultset.getWKT();
-
+	var markers = [];
+	
 	for (i=0;i < result.length;i++){
 		
 		//omnivore.wkt.parse('POLYGON((35 10, 45 45, 15 40, 10 20, 35 10),(20 30, 35 35, 30 20, 20 30))').bindLabel('Europe-Middle East	 Donut').addTo(map.LMap);
 	
 		omnivore.wkt.parse(result[i][1]).bindLabel(result[i][0]).addTo(map.LMap);
-
+		markers.push(omnivore.wkt.parse(result[i][1]));
 	}
+
+	var group = new L.featureGroup(markers);
+	map.LMap.fitBounds(group.getBounds());
 
 }
