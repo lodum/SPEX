7493a8345c697bc2aa3c556616ff7783180384c1
NemanjaKostic <kostic_nemanja@yahoo.com>
2014-06-17 15:20:06 +0200

    Update SpaceFilterPane.js

--- code/js/SpaceFilterPane.js
+++ code/js/SpaceFilterPane.js
@@ -7,7 +7,7 @@ function SpaceFilterPane() {
 SpaceFilterPane.prototype.displayGeometry = function(resultset){
 
 	for (i=0;i < resultset.getWKT().length;i++){
-		omnivore.wkt.parse(resultset.getWKT()[i][1]).bindLabel(resultset.getWKT()[i][0]).addTo(this.LMap);
+		omnivore.wkt.parse(resultset.getWKT()[i][1]).bindLabel(resultset.getWKT()[i][0]).addTo(map.LMap);
 	}
 
 
