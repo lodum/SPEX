1b055498b97c451158976965f8a3c0a35e339066
jimjonesbr <jimjonesbr@gmail.com>
2014-07-03 17:17:11 +0200

    displayGeometry now ignores CRS in WKT literals.

--- code/js/SpaceFilterPane.js
+++ code/js/SpaceFilterPane.js
@@ -13,10 +13,23 @@ SpaceFilterPane.prototype.displayGeometry = function(resultset){
 	for (i=0;i < result.length;i++){
 		
 		//omnivore.wkt.parse('POLYGON((35 10, 45 45, 15 40, 10 20, 35 10),(20 30, 35 35, 30 20, 20 30))').bindLabel('Europe-Middle East	 Donut').addTo(map.LMap);
-		
-		omnivore.wkt.parse(result[i][1]).bindLabel(result[i][0]).addTo(map.LMap);
-		markers.push(omnivore.wkt.parse(result[i][1]));
-		
+
+		//Checks if there is a CRS in the WKT literal
+		if(result[i][1].indexOf('<') === -1)
+		{
+			omnivore.wkt.parse(result[i][1]).bindLabel(result[i][0]).addTo(map.LMap);
+			markers.push(omnivore.wkt.parse(result[i][1]));
+		} else {
+
+			var geometryonly = result[i][1].substring(result[i][1].indexOf('>')+1, result[i][1].length); 
+			//console.log(result[i][0] + " @@@ " + geometryonly);
+
+			omnivore.wkt.parse(geometryonly.trim()).bindLabel(result[i][0]).addTo(map.LMap);
+			markers.push(omnivore.wkt.parse(geometryonly));
+
+			
+		}
+	
 	}
 
 	var group = new L.featureGroup(markers);
