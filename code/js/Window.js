84c494bc755ab3431a1059f293a5b18a29981aa0
gdbgauda <gdbgauda@gmail.com>
2014-07-06 00:54:53 +0200

    Update Window.js
    created Window.protorype.inside()

--- code/js/Window.js
+++ code/js/Window.js
@@ -9,3 +9,8 @@ Window.prototype.lowerLeftLongitude = 0.0;
 Window.prototype.variable = "";
 
 
+// window1.inside(window2) returns true, if window1 is inside window2.
+Window.prototype.inside=function(outerWindow){
+  if(this.upperRightLatitude<=outerWindow.upperRightLatitude && this.upperRightLongitude<=outerWindow.upperRightLongitude && this.lowerLeftLatitude>=outerWindow.lowerLeftLatitude && this.lowerLeftLongitude>=outerWindow.lowerLeftLongitude) return true;
+  else return false;
+}
