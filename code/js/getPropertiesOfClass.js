5d875ca67273e9b998f130fc6833befaf81ae4a2
gdbgauda <gdbgauda@gmail.com>
2014-07-05 16:52:05 +0200

    comments added

--- code/js/getPropertiesOfClass.js
+++ code/js/getPropertiesOfClass.js
@@ -1,4 +1,16 @@
-function getPropertiesOfClass(rdfClass,endpoint,myArray){
+/*
+Works like this: Given a class rdfClass="???",  an endpoint and an empty array, following query is executed at the given endpoint:
+
+select distinct ?predicate where{
+?x a "???".
+?x ?predicate ?y.
+filter(?predicate!=?rdf:type).
+}
+order by ?predicate
+
+Results are then extracted as strings out of the json returned by the endpoint, and stored in the given array.
+*/
+function getPropertiesOfClass(rdfClass,endpoint,resultsArray){
 
 
 //Formulate the Query in 'myQuery'
@@ -12,11 +24,12 @@ var myExecutor=new QueryExecutor();
 myExecutor.callback=function(str){
 	var jsonObj = eval('(' + str + ')');
 	for (var i=0; i < jsonObj.results.bindings.length ; i++){
-		myArray[i]=jsonObj.results.bindings[i].predicate.value;
+		resultsArray[i]=jsonObj.results.bindings[i].predicate.value;
 	}
 };
 
 myExecutor.executeQuery(myQuery,endpoint);
 
+//this line is only so that "tesGetPropertiesOfClass.html" displays the query. should be removed if used in another context
 document.getElementById("query").innerHTML = myQuery.serialiseQuery();
 }
