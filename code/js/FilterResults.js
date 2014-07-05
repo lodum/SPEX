bc07ad7a5384a2b966c68677d0baec27885b592f
gdbgauda <gdbgauda@gmail.com>
2014-07-06 01:05:30 +0200

    Update FilterResults.js

--- code/js/FilterResults.js
+++ code/js/FilterResults.js
@@ -2,24 +2,24 @@ function FilterResults(){}
 
 FilterResults.prototype.filterWKT=function(spexresultset){
   
-  //Get all the wkt-variables and their wkt which appear in spatial constraints:
-  //WKTvars contains two columns: [variable, wkt, window of spatial constraints]
+  //remove all the wkt-variables from resultset which appear in spatial constraints, and are not inside the constraint-windows
   var allWKT=spexresultset.getWKT();
   var WKTvars=[];
   var sc=spexresultset.spatialConstraints.slice();
   for(var i=0; i<allWKT.length; i++){
     var wktvar=allWKT[i][2];
-    var wkt=allWKT[i][1];
     if(wktvar!="__noWKT"){
       for(var j=0; j<sc.length; j++){
-        if(wktvar==sc[j].v){
-          WKTvars.push([wktvar,wkt,sc[j].w]);
+        if(wktvar==sc[j].v&& !boundingBox(allWKT[i][1]).inside(sc[j].w)){
+          //remove wktvar from spexresultset
           sc.splice(j,1);
         }
       }
     }
   }
   
+  function boundingBox(WKTpolygon){
+    //still to be done
+  }
   
-  
-}
+};
