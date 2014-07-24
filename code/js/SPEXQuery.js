9f52bcd09c7a30860c707e13c23becf339238f5a
NemanjaKostic <kostic_nemanja@yahoo.com>
2014-07-24 20:54:16 +0200

    Update SPEXQuery.js

--- code/js/SPEXQuery.js
+++ code/js/SPEXQuery.js
@@ -566,7 +566,7 @@ SPEXQuery.prototype.expandSpaceFilter = function(){
 
  	for (variable in this.spatialConstraints)  {
 		//if the variable is not a WKT variable
-		if(WKTvars.indexOf(variable) === -1) { 
+		if(WKTvars.indexOf(variable.substr(1)) === -1) { 
 			this.where(variable, "wgs84:lat", variable + "__lat")
 			.where("wgs84:long", variable + "__long");
 
