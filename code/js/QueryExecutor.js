c1d5465d22895655c9596266f74069e6c697c421
NemanjaKostic <kostic_nemanja@yahoo.com>
2014-07-25 21:24:08 +0200

    Update QueryExecutor.js

--- code/js/QueryExecutor.js
+++ code/js/QueryExecutor.js
@@ -114,8 +114,9 @@ QueryExecutor.prototype.sparqlQueryJson = function(queryStr, endpoint, timeout, 
   		//Detect spatially and temporally enabled variables and pass them on to the query pane.
   		console.log("detected spatial vars: " + JSON.stringify(results.detectSpatiallyEnabledVars()));
   		this.spatiallyEnabledVars = results.detectSpatiallyEnabledVars();
-  		//spex.ex.spatiallyEnabledVars = results.detectSpatiallyEnabledVars();
+  		spex.ex.spatiallyEnabledVars = results.detectSpatiallyEnabledVars();
   		console.log("query executor's spatial vars: " + JSON.stringify(this.spatiallyEnabledVars));
+  		console.log("spex.ex's spatial vars: " + JSON.stringify(spex.ex.spatiallyEnabledVars));
   		this.temporallyEnabledVars = results.detectTemporallyEnabledVars();
   		queryPane.setSpatialVars(this.spatiallyEnabledVars);
   		queryPane.setTemporalVars(this.temporallyEnabledVars);
