c468d6e33c07069c599b5c341246fd22057af61d
simonscheider <simonscheider@web.de>
2014-08-13 18:09:30 +0200

    event handler for highlighting results in different panes

---
+++ code/js/ResultItemEventHandler.js
@@ -0,0 +1,9 @@
+function ResultItemEventHandler(tao, tio, so){
+this.tableObject = tao;
+this.timeObject = tio;
+this.spaceObject so;
+}
+
+ResultItemEventHandler.prototype.constructor = ResultItemEventHandler;
+
+ResultItemEventHandler.prototype.highlight = function(){}
