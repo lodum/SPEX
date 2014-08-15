d89cb1d012d159841f0c1e20ce1f3bf2fd7b3186
gdbgauda <gdbgauda@gmail.com>
2014-08-15 18:43:43 +0200

    hopefully repaired damage?

--- code/js/spex.js
+++ code/js/spex.js
@@ -35,7 +35,15 @@ var spex = new function(){
 			//;
 			//.SPEXvariable("?c");
 			
-
+	Date.prototype.xsdDateTime = function() {
+		var yyyy = this.getFullYear().toString();
+		var mm = (this.getMonth()+1).toString(); // getMonth() is zero-based
+		var dd = this.getDate().toString();
+		var hh = this.getHours().toString();
+		var min = this.getMinutes().toString();
+		var sec = this.getSeconds().toString();
+		return yyyy + '-' + (mm[1]?mm:"0"+mm[0]) + '-' + (dd[1]?dd:"0"+dd[0]) + 'T' + (hh[1]?hh:"0"+hh[0]) + ':' + (min[1]?min:"0"+min[0]) + ':' + (sec[1]?sec:"0"+sec[0]); // padding
+	};
 	};
 	
 	this.showquery = function() {		 
