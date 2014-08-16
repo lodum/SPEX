36c6b466634a53f08b6409c3dfea273462cb11da
zimmp <peterzimmerhof@googlemail.com>
2014-08-16 08:39:43 +0200

    Recommited my changes

--- code/js/Time.js
+++ code/js/Time.js
@@ -6,4 +6,12 @@ Time.prototype.timeBeginning = 0.0;
 Time.prototype.timeEnd = 0.0;
 Time.prototype.variable = "";
 
-
+Date.prototype.xsdDateTime = function() {
+		var yyyy = this.getFullYear().toString();
+		var mm = (this.getMonth()+1).toString(); // getMonth() is zero-based
+		var dd = this.getDate().toString();
+		var hh = this.getHours().toString();
+		var min = this.getMinutes().toString();
+		var sec = this.getSeconds().toString();
+		return yyyy + '-' + (mm[1]?mm:"0"+mm[0]) + '-' + (dd[1]?dd:"0"+dd[0]) + 'T' + (hh[1]?hh:"0"+hh[0]) + ':' + (min[1]?min:"0"+min[0]) + ':' + (sec[1]?sec:"0"+sec[0]); // padding
+	};
--- code/js/spex.js
+++ code/js/spex.js
@@ -34,20 +34,10 @@ var spex = new function(){
 			//.SPEXvariable("?a")//.SPEXvariable("?b")
 			//;
 			//.SPEXvariable("?c");
-			
-	Date.prototype.xsdDateTime = function() {
-		var yyyy = this.getFullYear().toString();
-		var mm = (this.getMonth()+1).toString(); // getMonth() is zero-based
-		var dd = this.getDate().toString();
-		var hh = this.getHours().toString();
-		var min = this.getMinutes().toString();
-		var sec = this.getSeconds().toString();
-		return yyyy + '-' + (mm[1]?mm:"0"+mm[0]) + '-' + (dd[1]?dd:"0"+dd[0]) + 'T' + (hh[1]?hh:"0"+hh[0]) + ':' + (min[1]?min:"0"+min[0]) + ':' + (sec[1]?sec:"0"+sec[0]); // padding
-	};
 	};
 	
 	this.showquery = function() {		 
-		  document.getElementById("query").innerHTML = spex.q.getSPARQL();		 
+		  document.getElementById("query").innerHTML = spex.q.serialiseQuery();		 
 	};
 
 };
