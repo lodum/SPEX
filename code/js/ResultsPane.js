60d635008c9dae7f9484f0b2d99b9322d7336fd6
NemanjaKostic <kostic_nemanja@yahoo.com>
2014-10-13 18:25:13 +0200

    Revert 17fe7c1..5aaf2c9
    This rolls back to commit 17fe7c103aca05013c24fa56edad1a805df61c82.

--- code/js/ResultsPane.js
+++ code/js/ResultsPane.js
@@ -56,7 +56,7 @@ ResultsPane.prototype.display = function(spexresultset){
 		
 		//create table head and append it to the results table
 		var tableHead = document.createElement('thead');
-		tableHead.id = "result_header";
+		tableHead.id = "resultHeader";
 		var headRow = document.createElement('tr');
 		var userSelectedVars = spexresultset.getUserSelectedVariables();
 		$.each(userSelectedVars, function(varIndex, variable) { 
